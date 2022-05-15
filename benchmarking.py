1503d40c5350a9e44d28e4936cbd18e248430d90
Sam Varner <svarner@caltech.edu>
2022-05-15 16:07:57 -0700

    updated output structure and plotting for benchmarking

--- benchmarking.py
+++ benchmarking.py
@@ -6,18 +6,60 @@ import subprocess
 import os
 import re
 import numpy as np
+import csv
+from progressbar import ProgressBar
+pbar = ProgressBar()
+
+# Set figure parameters
+WIDTH = 1.5 * 8.3 / 2.54
+DOUBLE_WIDTH = 1.5 * 17.1 / 2.54
+DPI = 350
+format='png'
+matplotlib.rcParams.update(
+    {
+        'axes.labelsize': 14,
+        'axes.xmargin': 0,
+        'axes.ymargin': .1,
+        'lines.markersize': 3,
+        'figure.dpi': DPI,
+        'figure.autolayout': True,
+        'figure.figsize': (WIDTH, 3 * WIDTH / 4),
+        'figure.facecolor': 'white',
+        'font.size': 12,
+        'grid.color': '0',
+        'grid.linestyle': '-',
+        'legend.edgecolor': '1',
+        'legend.fontsize': 12,
+        'xtick.labelsize': 12,
+        'ytick.labelsize': 12,
+        'font.family': "DeJavu Serif",
+        'font.serif': ["Computer Modern Roman"],
+        'mathtext.fontset': 'cm',
+        'mathtext.rm': 'serif',
+        'text.usetex': False
+    }
+)
 
 
-n = np.logspace(1,3,50)
-n_iters = 1e3
-J = 0.5
+n = np.logspace(1,3,50).astype(int)
+n_iters = int(1e3)
+J = 0.1
 h = 0
-d = 2
+d = int(2)
+
+pc = "Sam"
+delim = "_"
+
+figure_root = "benchmarking/"+str(d)+"d/figures/"
+data_root = "benchmarking/"+str(d)+"d/data/"
+
+figure_name = pc+delim+str(n_iters)+delim+str(J)+delim+str(h)+delim+str(d)+"."+format
+data_name = pc+delim+str(n_iters)+delim+str(J)+delim+str(h)+delim+str(d)+".dat"
 
 t_cpp = np.zeros(len(n))
 t_julia = np.zeros(len(n))
 
-for i in range(len(n)):
+for i in pbar(range(len(n))):
     process_cpp = subprocess.run(["./cpp/Ising",str(n_iters), str(d), str(n[i]), str(J), str(h)], capture_output=True)
     t_cpp[i] = re.search('Program Time : (.*) seconds', str(process_cpp.stdout)).group(1)
     process_julia = subprocess.run(["julia Julia/benchmarking.jl "+str(int(n_iters))+" "+str(d)+" "+str(int(n[i]))+" "+str(J)+" "+str(h)], capture_output=True, shell = True)
@@ -28,6 +70,12 @@ plt.loglog(n,t_cpp,'.b', label = "C++")
 plt.loglog(n,t_julia,'.g', label = "Julia")
 plt.xlabel(r'$n$')
 plt.ylabel(r'$t / $s')
-plt.title(r'2D Benchmarks')
-plt.legend()
-plt.savefig('2D_benchmark.png')
+plt.title(rf'{d}D Benchmarks')
+plt.legend(fontsize=12, handlelength=1.25, columnspacing=1, labelspacing=0.25)
+plt.savefig(figure_root+figure_name)
+
+with open(data_root+data_name,'w',newline='') as f:
+    linewriter = csv.writer(f,delimiter=' ', quoting=csv.QUOTE_MINIMAL,dialect='unix')
+    for n_, tc, tj in zip(n, t_cpp, t_julia):
+                contents = [n_,tc,tj]
+                linewriter.writerow(contents)
