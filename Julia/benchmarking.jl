39e3c6590dc47a4c74f5979a56f342b5d91795d2
pw0908 <pw1017@ic.ac.uk>
2022-05-16 16:13:41 -0700

    Made a mistake in Julia benchmark

--- Julia/benchmarking.jl
+++ Julia/benchmarking.jl
@@ -36,8 +36,10 @@ J = parse(Float64, ARGS[4]); n_iters = parse(Int64, ARGS[1]); h= parse(Float64, 
 d = parse(Int64, ARGS[2])
 iter = 1:n_iters+1
 if d == 2
+    precompile(MCIsing,(Ising2DParam,))
     model = Ising2DParam(n, J, h, n_iters)
 else
+    precompile(MCIsing,(Ising3DParam,))
     model = Ising3DParam(n, J, h, n_iters)
 end
 
