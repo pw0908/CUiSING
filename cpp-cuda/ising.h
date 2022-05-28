5aa1a8593461236968807a72e63586eafd9e75db
Sam Varner <svarner@caltech.edu>
2022-05-28 03:33:21 -0700

    added and worked extensively on cpp-cuda, mc updates not implemented completely correctly

---
+++ cpp-cuda/ising.h
@@ -0,0 +1,19 @@
+#ifndef ISING_H
+#define ISING_H
+
+#include <chrono>
+#include <fstream>
+#include <getopt.h>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+#include <cuda_fp16.h>
+#include <curand.h>
+#include <cublas_v2.h>
+
+#include "cuda_helper.h"
+
+#define THREADS 256
+
+#endif
--- cpp-cuda/src/global_internal.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBALINTERNAL_H
-#define GLOBALINTERNAL_H
-
-/* Internal headers */
-#include "utilities/utilities.h"
-#include "Ising/Ising.h"
-
-#endif // GLOBALINTERNAL_H
