c94ff30f7acfb9b1e97434463ad83497417ae14c
pw0908 <pw1017@ic.ac.uk>
2022-06-01 13:41:26 -0700

    Fix benchmarking.jl for CUDA

--- julia-cuda/benchmarking.jl
+++ julia-cuda/benchmarking.jl
@@ -7,19 +7,19 @@ d = parse(Int64, ARGS[2])
 n_threads = 256
 iter = 1:n_iters+1
 if d == 2
-    precompile(MCIsing,(IsingCUDA2DParam,))
+    precompile(MCIsing,(CUDAIsing2DParam,))
     model = CUDAIsing2DParam(n, J, h, n_iters, n_threads)
 else
-    precompile(MCIsing,(IsingCUDA3DParam,))
+    precompile(MCIsing,(CUDAIsing3DParam,))
     model = CUDAIsing3DParam(n, J, h, n_iters, n_threads)
 end
-
+MCIsing(model)
 M, E = @time MCIsing(model)
 
-open("output/julia_cpu_output.dat", "w") do f
-    for i = iter
-        if mod(i,10) == 0
-            println(f, i," ",@sprintf("%.5f",E[i])," ", @sprintf("%.5f",M[i]))
-        end
-    end
-end
+# open("output/julia_cpu_output.dat", "w") do f
+#     for i = iter
+#         if mod(i,10) == 0
+#             println(f, i," ",@sprintf("%.5f",E[i])," ", @sprintf("%.5f",M[i]))
+#         end
+#     end
+# end
