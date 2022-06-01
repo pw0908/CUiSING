c42f96af6cca2a3fff71f509c8b98c061629041e
pw0908 <pw1017@ic.ac.uk>
2022-05-31 19:56:11 -0700

    Clean implementation of Ising.jl

--- julia-cuda/Ising.jl
+++ julia-cuda/Ising.jl
@@ -13,6 +13,11 @@ struct CUDAIsing2DParam <: CUDAIsing2DModel
     h::Float64
     n_iters::Int64
     n_threads::Int64
+    N::Int64
+    function CUDAIsing2DParam(n::Int64,J::Float64,h::Float64,
+                              n_iters::Int64,n_threads::Int64)
+        return new(n,J,h,n_iters,n_threads,n^2)
+    end
 end
 
 struct CUDAIsing3DParam <: CUDAIsing3DModel
@@ -21,12 +26,37 @@ struct CUDAIsing3DParam <: CUDAIsing3DModel
     h::Float64
     n_iters::Int64
     n_threads::Int64
+    N::Int64
+    function CUDAIsing3DParam(n::Int64,J::Float64,h::Float64,
+        n_iters::Int64,n_threads::Int64)
+        return new(n,J,h,n_iters,n_threads,n^3)
+    end
 end 
 
+function InitialiseIsing(model::CUDAIsingModel)
+    N = model.N
+    n_iters = model.n_iters
+    n_threads = model.n_threads
+    n_blocks = Int64(floor((N+n_threads-1)/n_threads))
+
+    ms = CUDA.zeros(Float64,n_iters + 1) 
+    Es = CUDA.zeros(Float64,n_iters + 1)
+
+    rng = CURAND.RNG(CURAND.CURAND_RNG_PSEUDO_PHILOX4_32_10)
+    Random.seed!(rng,Int64(floor(time())))
+
+    rands = Random.rand(rng, Float64, N)
+
+    lattice = CuArray{Int}(undef,N)
+    @cuda threads=n_threads blocks=n_blocks init_lattice!(model,lattice,rands)
+    device_synchronize()
+    return lattice, rng, ms, Es
+end
+
 function init_lattice!(model::CUDAIsingModel,lattice,rands)
-    n = model.n
+    N = model.N
     tid = blockDim().x*(blockIdx().x-1) + threadIdx().x
-    if tid >= floor(n^2+1)
+    if tid >= floor(N+1)
         return 
     end
     r = rands[tid];
@@ -38,55 +68,12 @@ function init_lattice!(model::CUDAIsingModel,lattice,rands)
     return
 end
 
-function MCIsing(model::CUDAIsingModel)
-    n_threads = model.n_threads
-    n_iters = model.n_iters
-
-    # Initialize the vectors for storing magnetization and energy
-    ms = CUDA.zeros(Float64,n_iters + 1) 
-    Es = CUDA.zeros(Float64,n_iters + 1)
-
-    lattice, rng, n_blocks = InitialiseIsing(model)
-    calcMagnetisation!(model,lattice, ms, 1)
-    calcHamiltonian!(model,lattice, Es, 1)
-    # println(Es)
-
-    for l ∈ 2:n_iters + 1
-        # In each MC iteration, attempt to flip all spins, using metropolis
-        # println(l)
-        IsingIter!(model,lattice,rng,n_blocks)
-        
-        # Calculate observables
-        calcMagnetisation!(model, lattice, ms, l)
-        calcHamiltonian!(model, lattice, Es, l)
-    end
-    return ms, Es
-end
-
-function InitialiseIsing(model::CUDAIsing2DModel)
-    n = model.n
+function calcHamiltonian!(model::CUDAIsingModel,lattice,E,iter)
+    N = model.N
     n_iters = model.n_iters
     n_threads = model.n_threads
 
-    n_blocks = Int64(floor((n^2+n_threads-1)/n_threads))
-
-    rng = CURAND.RNG(CURAND.CURAND_RNG_PSEUDO_PHILOX4_32_10)
-    Random.seed!(rng,Int64(floor(time())))
-
-    rands = Random.rand(rng, Float64, n^2)
-
-    lattice = CuArray{Int}(undef,n^2)
-    @cuda threads=n_threads blocks=n_blocks init_lattice!(model,lattice,rands)
-    device_synchronize()
-    return lattice, rng, n_blocks
-end
-
-function calcHamiltonian!(model::CUDAIsing2DModel,lattice,E,iter)
-    n = model.n
-    n_iters = model.n_iters
-    n_threads = model.n_threads
-
-    n_blocks = Int64(floor((n^2+n_threads-1)/n_threads))
+    n_blocks = Int64(floor((N+n_threads-1)/n_threads))
     @cuda threads=n_threads blocks=n_blocks shmem=n_threads*sizeof(Float64) calcHamiltonianKernel!(model,lattice,E,iter)
 end
 
@@ -110,10 +97,9 @@ function calcHamiltonianKernel!(model::CUDAIsing2DModel,lattice,E,iter)
         @inbounds sr = lattice[(i-1)*n+(n+j%n)%n+1]
         @inbounds su = lattice[(n+i%n)%n*n+j]
         @inbounds sd = lattice[(n+(i-2)%n)%n*n+j]
-
-        @inbounds sum_spins = sl+sr+su+sd
         @inbounds sij = lattice[idx]
-        @inbounds s[tid] -= sij*(J*sum_spins/2+h)
+
+        @inbounds s[tid] -= sij*(J*(sl+sr+su+sd)/2+h)
     end
     sync_threads()
 
@@ -133,16 +119,7 @@ function calcHamiltonianKernel!(model::CUDAIsing2DModel,lattice,E,iter)
     return
 end 
 
-function calcMagnetisation!(model::CUDAIsing2DModel,lattice,m,iter)
-    n = model.n
-    n_iters = model.n_iters
-    n_threads = model.n_threads
-
-    n_blocks = Int64(floor((n^2+n_threads-1)/n_threads))
-    @cuda threads=n_threads blocks=n_blocks shmem=n_threads*sizeof(Float64) calcMagnetisationKernel!(model,lattice,m,iter)
-end
-
-function calcMagnetisationKernel!(model::CUDAIsing2DModel,lattice,m,iter)
+function calcHamiltonianKernel!(model::CUDAIsing3DModel,lattice,E,iter)
     n = model.n
     J = model.J
     h = model.h
@@ -152,7 +129,62 @@ function calcMagnetisationKernel!(model::CUDAIsing2DModel,lattice,m,iter)
     tid = threadIdx().x
     start = tid + (blockIdx().x-1)*blockDim().x
     stride = blockDim().x*gridDim().x
-    stop = n^2
+    stop = n^3-1
+    s[tid] = 0.0;
+
+    for idx ∈ start:stride:stop
+        @inbounds i = Int64(floor((idx-1) / (n*n)))+1
+        @inbounds j = Int64(floor(mod((idx-1)/n,n)))+1
+        @inbounds k = Int64(mod((idx-1),n))+1
+
+        @inbounds sl = lattice[(i-1)*n*n+(n+(j-2)%n)%n*n+(k-1)+1];
+        @inbounds sr = lattice[(i-1)*n*n+(n+j%n)%n*n+(k-1)+1];
+        @inbounds su = lattice[(n+i%n)%n*n*n+(j-1)*n+(k-1)+1];
+        @inbounds sd = lattice[(n+(i-2)%n)%n*n*n+(j-1)*n+(k-1)+1];
+        @inbounds sn = lattice[(i-1)*n*n+j*n+(n+(k-2)%n)%n+1];
+        @inbounds ss = lattice[(i-1)*n*n+j*n+(n+k%n)%n+1];
+        @inbounds sijk = lattice[idx];
+
+        @inbounds s[tid] -= sijk*(J*(sl+sr+su+sd+sn+ss)/2+h)
+    end
+    sync_threads()
+
+    j =  Int64(floor(blockDim().x/2))
+
+    while j > 0
+        if tid<j+1
+            s[tid] += s[tid+j]
+        end
+        sync_threads()
+        j = j>>1
+    end
+
+    if threadIdx().x==1
+        CUDA.@atomic E[iter] += s[1]/((J*3+abs(h))*n^2)
+    end
+    return
+end
+
+function calcMagnetisation!(model::CUDAIsingModel,lattice,m,iter)
+    N = model.N
+    n_iters = model.n_iters
+    n_threads = model.n_threads
+
+    n_blocks = Int64(floor((N+n_threads-1)/n_threads))
+    @cuda threads=n_threads blocks=n_blocks shmem=n_threads*sizeof(Float64) calcMagnetisationKernel!(model,lattice,m,iter)
+end
+
+function calcMagnetisationKernel!(model::CUDAIsingModel,lattice,m,iter)
+    N = model.N
+    J = model.J
+    h = model.h
+    n_threads = model.n_threads
+    s = CuDynamicSharedArray(Float64,n_threads)
+
+    tid = threadIdx().x
+    start = tid + (blockIdx().x-1)*blockDim().x
+    stride = blockDim().x*gridDim().x
+    stop = N
     s[tid] = 0.0;
 
     for idx ∈ start:stride:stop
@@ -172,15 +204,17 @@ function calcMagnetisationKernel!(model::CUDAIsing2DModel,lattice,m,iter)
     end
 
     if threadIdx().x==1
-        CUDA.@atomic m[iter] += s[1]/n^2
+        CUDA.@atomic m[iter] += s[1]/N
     end
     return
 end 
 
-function IsingIter!(model::CUDAIsing2DModel,lattice,rng, n_blocks)
-    n = model.n
+function IsingIter!(model::CUDAIsingModel,lattice,rng)
+    N = model.N
     n_threads = model.n_threads
-    rands = Random.rand(rng, Float64, n^2)
+    n_blocks = Int64(floor((N+n_threads-1)/n_threads))
+
+    rands = Random.rand(rng, Float64, N)
     @cuda threads=n_threads blocks=n_blocks IsingIterKernel!(model,false,lattice,rands)
     @cuda threads=n_threads blocks=n_blocks IsingIterKernel!(model,true,lattice,rands)
 end
@@ -193,6 +227,7 @@ function IsingIterKernel!(model::CUDAIsing2DModel,sublattice,lattice,rands)
     tid = blockDim().x*(blockIdx().x-1)+threadIdx().x
     i = Int64(floor((tid-1) / n))+1
     j = Int64(mod((tid-1),n))+1
+
     if tid>=n^2
         return
     elseif ((i-1)%2 != (j-1)%2) != sublattice
@@ -203,12 +238,42 @@ function IsingIterKernel!(model::CUDAIsing2DModel,sublattice,lattice,rands)
     @inbounds sr = lattice[(i-1)*n+(n+j%n)%n+1]
     @inbounds su = lattice[(n+i%n)%n*n+j]
     @inbounds sd = lattice[(n+(i-2)%n)%n*n+j]
+    @inbounds sij = lattice[tid];
 
-    sum_spins = sl+sr+su+sd
-    s = lattice[tid]
-    boltz = exp(-2*s*(sum_spins*J+h))
+    boltz = exp(-2*sij*((sl+sr+su+sd)*J+h))
     if (rands[tid]<=boltz)
-        @inbounds lattice[tid]=-s
+        @inbounds lattice[tid]=-sij
+    end
+    return
+end
+
+function IsingIterKernel!(model::CUDAIsing3DModel,sublattice,lattice,rands)
+    n = model.n
+    J = model.J 
+    h = model.h
+
+    tid = blockDim().x*(blockIdx().x-1)+threadIdx().x
+    i = Int64(floor((tid-1) / (n*n)))+1
+    j = Int64(floor(mod((tid-1)/n,n)))+1
+    k = Int64(mod((tid-1),n))+1
+    
+    if tid>=n^3
+        return
+    elseif ((k%2 == j%2) != i%2) != sublattice
+        return
+    end
+
+    @inbounds sl = lattice[(i-1)*n*n+(n+(j-2)%n)%n*n+(k-1)+1];
+    @inbounds sr = lattice[(i-1)*n*n+(n+j%n)%n*n+(k-1)+1];
+    @inbounds su = lattice[(n+i%n)%n*n*n+(j-1)*n+(k-1)+1];
+    @inbounds sd = lattice[(n+(i-2)%n)%n*n*n+(j-1)*n+(k-1)+1];
+    @inbounds sn = lattice[(i-1)*n*n+j*n+(n+(k-2)%n)%n+1];
+    @inbounds ss = lattice[(i-1)*n*n+j*n+(n+k%n)%n+1];
+    @inbounds sijk = lattice[tid];
+
+    boltz = exp(-2*sijk*((sl+sr+su+sd+sn+ss)*J+h))
+    if (rands[tid]<=boltz)
+        @inbounds lattice[tid]=-sijk
     end
     return
 end
--- julia/Ising.jl
+++ julia/Ising.jl
@@ -9,6 +9,11 @@ struct Ising2DParam <: Ising2DModel
     J::Float64
     h::Float64
     n_iters::Int64
+    N::Int64
+    function Ising2DParam(n::Int64,J::Float64,h::Float64,
+        n_iters::Int64)
+        return new(n,J,h,n_iters,n^2)
+    end
 end
 
 struct Ising3DParam <: Ising3DModel
@@ -16,110 +21,123 @@ struct Ising3DParam <: Ising3DModel
     J::Float64
     h::Float64
     n_iters::Int64
+    N::Int64
+    function Ising3DParam(n::Int64,J::Float64,h::Float64,
+        n_iters::Int64)
+        return new(n,J,h,n_iters,n^3)
+    end
 end 
 
 function MCIsing(model::IsingModel)
     n_iters = model.n_iters
-    
-    # Initialize the vectors for storing magnetization and energy
-    ms = zeros(n_iters + 1, 1) 
-    Es = zeros(n_iters + 1, 1)
 
     # Initialize matrix of spins (randomly assigned +-1)
-    A,N = InitialiseIsing(model) 
+    lattice,rng,ms,Es = InitialiseIsing(model) 
 
     # Calculate initial m and E
-    ms[1] = calcMagnetisation(model, A, N)
-    Es[1] = calcHamiltonian(model, A)
+    calcMagnetisation!(model, lattice, ms, 1)
+    calcHamiltonian!(model, lattice, Es, 1)
     
     # Outer loop for MC iterations
     for l ∈ 2:n_iters + 1
         # In each MC iteration, attempt to flip all spins, using metropolis
-        IsingIter!(model,A)
+        IsingIter!(model,lattice,rng)
         
         # Calculate observables
-        ms[l] = calcMagnetisation(model,A, N)
-        Es[l] = calcHamiltonian(model, A)
+        calcMagnetisation!(model, lattice, ms, l)
+        calcHamiltonian!(model, lattice, Es, l)
     end
     return ms, Es
 end
 
-function calcMagnetisation(model::IsingModel,A,N)
-    return sum(A) / N
+function calcMagnetisation!(model::IsingModel,lattice,m,iter)
+    m[iter] = sum(lattice) / model.N
 end
 
 function InitialiseIsing(model::Ising2DModel)
+    n_iters = model.n_iters
     n = model.n
-    return 1 .- 2 * rand([0, 1], (n,n)), n*n
+
+    rng = Xoshiro(Int64(floor(time())))
+
+    ms = zeros(n_iters + 1, 1) 
+    Es = zeros(n_iters + 1, 1)
+    return 1 .- 2 * rand([0, 1], (n,n)), rng, ms, Es
 end
 
-function calcHamiltonian(model::Ising2DModel,A)
-    E = 0.
+function calcHamiltonian!(model::Ising2DModel,lattice,E,iter)
+    Es = 0.
     n = model.n
     J = model.J
     h = model.h
     for i ∈ 1:n, j ∈ 1:n
-        E -= J*A[i,j]*(A[i,mod(n+mod(j,n),n)+1]+A[i,mod(n+mod(j-2,n),n)+1]+
-                       A[mod(n+mod(i,n),n)+1,j]+A[mod(n+mod(i-2,n),n)+1,j])/2.0
-        E -= h*A[i,j]
+        Es -= J*lattice[i,j]*(lattice[i,mod(n+mod(j,n),n)+1]+lattice[i,mod(n+mod(j-2,n),n)+1]+
+                       lattice[mod(n+mod(i,n),n)+1,j]+lattice[mod(n+mod(i-2,n),n)+1,j])/2.0
+        Es -= h*lattice[i,j]
     end
-    return E/((J*2+abs(h))*n^2)
+    E[iter] = Es/((J*2+abs(h))*n^2)
 end 
 
-function calcDeltaHamiltonian(model::Ising2DModel,A,i,j)
+function calcDeltaHamiltonian(model::Ising2DModel,lattice,i,j)
     n = model.n
     J = model.J
     h = model.h
-    return 2.0*(J*A[i,j]*(A[i,mod((n+mod(j,n)),n)+1]+A[i,mod((n+mod(j-2,n)),n)+1]+
-                          A[mod((n+mod(i,n)),n)+1,j]+A[mod((n+mod(i-2,n)),n)+1,j])+
-                          A[i,j]*h)
+    return 2.0*(J*lattice[i,j]*(lattice[i,mod((n+mod(j,n)),n)+1]+lattice[i,mod((n+mod(j-2,n)),n)+1]+
+                          lattice[mod((n+mod(i,n)),n)+1,j]+lattice[mod((n+mod(i-2,n)),n)+1,j])+
+                          lattice[i,j]*h)
 end
 
-function IsingIter!(model::Ising2DModel,A)
+function IsingIter!(model::Ising2DModel,lattice,rng)
     n = model.n
     for i ∈ 1:n, j ∈ 1:n
-        dE = calcDeltaHamiltonian(model, A, i, j)
-        if dE <= 0 || rand() <= exp(-dE)
-            A[i,j] *= -1
+        dE = calcDeltaHamiltonian(model, lattice, i, j)
+        if dE <= 0 || rand(rng) <= exp(-dE)
+            lattice[i,j] *= -1
         end
     end
 end
 
 function InitialiseIsing(model::Ising3DModel)
+    n_iters = model.n_iters
     n = model.n
-    return 1 .- 2 * rand([0, 1], (n,n,n)),n*n*n
+
+    rng = Xoshiro(Int64(floor(time())))
+
+    ms = zeros(n_iters + 1, 1) 
+    Es = zeros(n_iters + 1, 1)
+    return 1 .- 2 * rand([0, 1], (n,n,n)), rng, ms, Es
 end
 
-function calcHamiltonian(model::Ising3DModel,A)
+function calcHamiltonian(model::Ising3DModel,lattice)
     E = 0.
     n = model.n
     J = model.J
     h = model.h
     for i ∈ 1:n, j ∈ 1:n, k ∈ 1:n
-        E -= J*A[i,j,k]*(A[i,mod(n+mod(j,n),n)+1,k]+A[i,mod(n+mod(j-2,n),n)+1,k]+
-                         A[mod(n+mod(i,n),n)+1,j,k]+A[mod(n+mod(i-2,n),n)+1,j,k]+
-                         A[i,j,mod(n+mod(k,n),n)+1]+A[i,j,mod(n+mod(k-2,n),n)+1])/2.0
-        E -= h*A[i,j,k]
+        E -= J*lattice[i,j,k]*(lattice[i,mod(n+mod(j,n),n)+1,k]+lattice[i,mod(n+mod(j-2,n),n)+1,k]+
+                         lattice[mod(n+mod(i,n),n)+1,j,k]+lattice[mod(n+mod(i-2,n),n)+1,j,k]+
+                         lattice[i,j,mod(n+mod(k,n),n)+1]+lattice[i,j,mod(n+mod(k-2,n),n)+1])/2.0
+        E -= h*lattice[i,j,k]
     end
     return E/((J*3+abs(h))*n^3)
 end 
 
-function calcDeltaHamiltonian(model::Ising3DModel,A,i,j,k)
+function calcDeltaHamiltonian(model::Ising3DModel,lattice,i,j,k)
     n = model.n
     J = model.J
     h = model.h
-    return 2.0*(J*A[i,j,k]*(A[i,mod((n+mod(j,n)),n)+1,k]+A[i,mod((n+mod(j-2,n)),n)+1,k]+
-                            A[mod((n+mod(i,n)),n)+1,j,k]+A[mod((n+mod(i-2,n)),n)+1,j,k]+
-                            A[i,j,mod((n+mod(k,n)),n)+1]+A[i,j,mod((n+mod(k-2,n)),n)+1])+
-                            A[i,j,k]*h)
+    return 2.0*(J*lattice[i,j,k]*(lattice[i,mod((n+mod(j,n)),n)+1,k]+lattice[i,mod((n+mod(j-2,n)),n)+1,k]+
+                            lattice[mod((n+mod(i,n)),n)+1,j,k]+lattice[mod((n+mod(i-2,n)),n)+1,j,k]+
+                            lattice[i,j,mod((n+mod(k,n)),n)+1]+lattice[i,j,mod((n+mod(k-2,n)),n)+1])+
+                            lattice[i,j,k]*h)
 end
 
-function IsingIter!(model::Ising3DModel,A)
+function IsingIter!(model::Ising3DModel,lattice,rng)
     n = model.n
     for i ∈ 1:n, j ∈ 1:n, k ∈ 1:n
-        dE = calcDeltaHamiltonian(model, A, i, j, k)
-        if dE <= 0 || rand() <= exp(-dE)
-            A[i,j,k] *= -1
+        dE = calcDeltaHamiltonian(model, lattice, i, j, k)
+        if dE <= 0 || rand(rng) <= exp(-dE)
+            lattice[i,j,k] *= -1
         end
     end
 end
